fb42d971ecee0cf39a455be12e656e45401e59fb
hersonhebert <hersonhebert@hotmail.com>
2025-03-05 12:20:03 -0300

    Update

--- R/zzz.R
+++ R/zzz.R
@@ -1,19 +1,32 @@
+#.onLoad <- function(libname, pkgname) {
+#  # Carrega o pacote reticulate
+#  if (!requireNamespace("reticulate", quietly = TRUE)) {
+#    stop("Reticulate not Found.")
+#  }
+#  
+#  # Verifica se o Python esta disponível
+#  if (!reticulate::py_available(initialize = TRUE)) {
+#    stop("Python not found.")
+#  }
+#  
+#  if (!reticulate::virtualenv_exists("fibos_venv")) {
+#    reticulate::virtualenv_create("fibos_venv")
+#    reticulate::virtualenv_install("fibos_venv", packages = "fibos")
+#  }
+#  
+#  # Configura o uso do ambiente virtual
+#  reticulate::use_virtualenv("fibos_venv", required = TRUE)
+#}
+
 .onLoad <- function(libname, pkgname) {
   # Carrega o pacote reticulate
   if (!requireNamespace("reticulate", quietly = TRUE)) {
-    stop("Reticulate not Found.")
+    stop("O pacote 'reticulate' é necessário, mas não está instalado.")
   }
   
-  # Verifica se o Python esta disponível
-  if (!reticulate::py_available(initialize = TRUE)) {
-    stop("Python not found.")
-  }
+  # Declara a dependência do pacote Python 'fibos'
+  reticulate::py_require("fibos")
   
-  if (!reticulate::virtualenv_exists("fibos_venv")) {
-    reticulate::virtualenv_create("fibos_venv")
-    reticulate::virtualenv_install("fibos_venv", packages = "fibos")
-  }
-  
-  # Configura o uso do ambiente virtual
-  reticulate::use_virtualenv("fibos_venv", required = TRUE)
+  # Importa o módulo Python 'fibos' com carregamento adiado
+  fibos <<- reticulate::import("fibos", delay_load = TRUE)
 }
